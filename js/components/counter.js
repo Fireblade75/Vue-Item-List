c90bf2abf8f596f78156c261b225ed337106e04d
Glitch (open-society) <none>
2017-11-26 15:32:34 +0000

    Checkpoint

--- js/components/counter.js
+++ js/components/counter.js
@@ -5,15 +5,18 @@ class Counter extends React.Component {
   render() {
     return (
       <div className="counter">
-        <div className="row">
+        <div className="score-row">
           <span>Team A</span>
           <span>{this.props.homeScore}</span>
           <button className="btn">Goal!</button>
         </div>
-        <div className="row">
+        <div className="score-row">
           <span>Team B</span>
           <span>{this.props.outScore}</span>
           <button className="btn">Goal!</button>
+        </div>
+        <div className="reset-row">
+          <button className="btn">Reset Scores</button>
         </div>
       </div>
     )

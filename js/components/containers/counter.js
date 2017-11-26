e37859eefcbf33fcc3fd4aa6504a1dd40664354a
Glitch (open-society) <none>
2017-11-26 15:52:35 +0000

    Checkpoint

--- js/components/containers/counter.js
+++ js/components/containers/counter.js
@@ -2,11 +2,32 @@ import React from 'react';
 import {connect} from 'react-redux';
 import Counter from '../counter';
 
+
 const mapStateToProps = state => {
   return {
     homeScore: state.homeScore,
-    outScore: state.outScore
+    outScore: state.outScore,
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addHomeScore() {
+      dispatch(
+        setLoginUsername(username)
+      );
+    },
+    addOutScore() {
+      dispatch(
+        setLoginPassword(password)
+      );
+    },
+    clearScores() {
+      dispatch(
+        setLoginPassword(password)
+      );
+    }
+  }
+} 
   
 export default connect(mapStateToProps)(Counter);
